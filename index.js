ce91146a33b60ba2114fa2ffb5a73fa8b3a6d30a
ivi0540 <ivi0540@gmail.com>
2023-08-08 12:34:25 +0300

    feat: problem solved

---
+++ index.js
@@ -0,0 +1,9 @@
+function reverse(num) {
+    let str = String(num);
+    let result = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        result = result + str[i];
+    };
+    return Number(result);
+};
+// console.log(reverse(1994));
