d0dd85b3beb657d6f52a8e3473269bbb4f22208d
ivi0540 <ivi0540@gmail.com>
2023-08-09 12:29:30 +0300

    fear: work in another directory

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-function reverse(num) {
-    let str = String(num);
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    };
+    let str = String(n);
     let result = "";
     for (let i = str.length - 1; i >= 0; i--) {
         result = result + str[i];
     };
     return Number(result);
-};
-console.log(reverse(123));
-console.log(reverse(233));
-console.log(reverse(535));
-console.log(reverse(95034));
+}
